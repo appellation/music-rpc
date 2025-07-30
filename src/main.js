a6afb606da9ee6a61f8c7a7d5f94c975ff213c6d
Will Nelson <will.nelson@discordapp.com>
2025-07-30 08:27:03 -0700

    add biome config

--- src/main.js
+++ src/main.js
@@ -3,28 +3,28 @@ import { listen } from "@tauri-apps/api/event";
 import { enable, disable, isEnabled } from "@tauri-apps/plugin-autostart";
 
 async function getMedia() {
-  const media = await invoke("get_media");
-  await setActivity(media);
+	const media = await invoke("get_media");
+	await setActivity(media);
 }
 
 async function setActivity(media) {
-  if (media) {
-    document.getElementById("artwork").style = "display: block";
-    document.getElementById("artwork").src =
-      `data:${media.artwork_mime};base64,${media.artwork_bytes}`;
-  } else {
-    document.getElementById("artwork").style = "display: none";
-  }
+	if (media) {
+		document.getElementById("artwork").style = "display: block";
+		document.getElementById("artwork").src =
+			`data:${media.artwork_mime};base64,${media.artwork_bytes}`;
+	} else {
+		document.getElementById("artwork").style = "display: none";
+	}
 
-  document.getElementById("title").innerText = media?.title ?? "";
-  document.getElementById("artist").innerText = media?.artist ?? "";
-  await invoke("set_activity", { media });
+	document.getElementById("title").innerText = media?.title ?? "";
+	document.getElementById("artist").innerText = media?.artist ?? "";
+	await invoke("set_activity", { media });
 }
 
 getMedia().catch(console.error);
 
 listen("media_change", ({ payload }) => {
-  setActivity(payload);
+	setActivity(payload);
 });
 
 invoke("subscribe_media").catch(console.error);
@@ -32,6 +32,6 @@ invoke("subscribe_media").catch(console.error);
 const autostartCheckbox = document.getElementById("autostart");
 autostartCheckbox.checked = await isEnabled();
 autostartCheckbox.addEventListener("change", (event) => {
-  if (event.target.checked) enable();
-  else disable();
+	if (event.target.checked) enable();
+	else disable();
 });
--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,43 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // prevent vite from obscuring rust errors
-  clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
-  server: {
-    strictPort: true,
-  },
-  // to access the Tauri environment variables set by the CLI with information about the current target
-  envPrefix: [
-    "VITE_",
-    "TAURI_PLATFORM",
-    "TAURI_ARCH",
-    "TAURI_FAMILY",
-    "TAURI_PLATFORM_VERSION",
-    "TAURI_PLATFORM_TYPE",
-    "TAURI_DEBUG",
-  ],
-  build: {
-    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    // target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
-    // TODO: remove this once Tidal stops using top level await?
-    target: "esnext",
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
-  esbuild: {
-    // target: ["es2022", "chrome115"],
-    supported: {
-      "top-level-await": true,
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      supported: {
-        "top-level-await": true,
-      },
-    },
-  },
+	// prevent vite from obscuring rust errors
+	clearScreen: false,
+	// Tauri expects a fixed port, fail if that port is not available
+	server: {
+		strictPort: true,
+	},
+	// to access the Tauri environment variables set by the CLI with information about the current target
+	envPrefix: [
+		"VITE_",
+		"TAURI_PLATFORM",
+		"TAURI_ARCH",
+		"TAURI_FAMILY",
+		"TAURI_PLATFORM_VERSION",
+		"TAURI_PLATFORM_TYPE",
+		"TAURI_DEBUG",
+	],
+	build: {
+		// Tauri uses Chromium on Windows and WebKit on macOS and Linux
+		// target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
+		// TODO: remove this once Tidal stops using top level await?
+		target: "esnext",
+		// don't minify for debug builds
+		minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+		// produce sourcemaps for debug builds
+		sourcemap: !!process.env.TAURI_DEBUG,
+	},
+	esbuild: {
+		// target: ["es2022", "chrome115"],
+		supported: {
+			"top-level-await": true,
+		},
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			supported: {
+				"top-level-await": true,
+			},
+		},
+	},
 });
